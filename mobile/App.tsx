768f78d4c9137f9b3f5744080944a8cb9d8cfbb9
João Lucas <lucasbanana96@gmail.com>
2022-11-04 01:27:48 -0300

    pools update

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar} from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from  '@expo-google-fonts/roboto';
 
-import { Pools } from './src/screens/Pools';
+import { SignIn } from './src/screens/Signin';
 import { Loading } from './src/components/Loading';
 
 import { AuthContextProvider } from "./src/contexts/AuthContext";
@@ -22,7 +22,7 @@ export default function App() {
           translucent
         />
         
-        { fontsLoaded ? <Pools/> : <Loading/> } 
+        { fontsLoaded ? <SignIn/> : <Loading/> } 
       </AuthContextProvider>
     </NativeBaseProvider>
   );
